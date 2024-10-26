697b66e00b68d6fa8b63e23961906afabebdd1c6
lahiru <lahirunisaluor123@gmail.com>
2024-10-26 10:45:23 +0530

    created explore categories(wihtout functions)

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ExploreMenu.css';
+import { category_list } from '../../assets/assets';
+
+const ExploreMenu = () => {
+    return (
+        <div className='explore-menu' id='explore-menu'>
+            <hr/>
+            <h1>Explore Our Collection</h1>
+            <p className='explore-menu-text'>Browse our curated selection of furniture and home equipment, designed to blend style with functionality. Use our augmented reality feature to visualize each piece in your home, ensuring the perfect fit for your space and style preferences.</p>
+            <div className='explore-menu-categories'>
+                {category_list.map((item, index) => {  
+                    return (
+                        <div key={index} className='explore-menu-category-item'>
+                            <img src={item.category_image} alt={item.category_name} />
+                            <p>{item.category_name}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default ExploreMenu;
