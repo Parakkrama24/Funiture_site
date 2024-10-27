efe75b222b84857a4fe64bff0e1a2498226fd21a
lahiru <lahirunisaluor123@gmail.com>
2024-10-28 05:07:17 +0530

    css update in explore collection in home page

--- src/components/ExploreMenuHeader/ExploreMenuHeader.jsx
+++ src/components/ExploreMenuHeader/ExploreMenuHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ExploreMenuHeader.css';
+import { assets } from '../../assets/assets';
 
 const ExploreMenu = () => {
     return (
@@ -9,6 +10,10 @@ const ExploreMenu = () => {
             <p className='explore-menu-text'>
                 Browse our curated selection of furniture and home equipment, designed to blend style with functionality. Use our augmented reality feature to visualize each piece in your home, ensuring the perfect fit for your space and style preferences.
             </p>
+            <button className='explore-button'>
+                Explore Our Collection 
+                <img src={assets.arrow} alt="Arrow" className="arrow-image" />
+            </button>
             
         </div>
     );
