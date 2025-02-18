98095fa6c9936a7088adc019fa0beef9edfe625f
Neshadi <neshadihirunika@gmail.com>
2025-02-18 19:54:03 +0530

    Updated firebaseconfig.jsx

--- src/FireBase/firebaseConfig.js
+++ src/FireBase/firebaseConfig.js
@@ -1,20 +1,21 @@
-// src/firebase.js
+src/firebase.js
 
-// // Replace with your actual Firebase config object from Firebase Console
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDa5lkKbThEfRpUDk2iK3ZaQv71NIe6ZdE",
-//   authDomain: "sproject-d6684.firebaseapp.com",
-//   projectId: "sproject-d6684",
-//   storageBucket: "sproject-d6684.appspot.com",
-//   messagingSenderId: "250439299647",
-//   appId: "1:250439299647:web:f0c707d1d9de30f2227e8a",
-//   measurementId: "G-L85LF5JSP9"
-// };
+// Replace with your actual Firebase config object from Firebase Console
+const firebaseConfig = {
+  apiKey: "AIzaSyDa5lkKbThEfRpUDk2iK3ZaQv71NIe6ZdE",
+  authDomain: "sproject-d6684.firebaseapp.com",
+  projectId: "sproject-d6684",
+  storageBucket: "sproject-d6684.appspot.com",
+  messagingSenderId: "250439299647",
+  appId: "1:250439299647:web:f0c707d1d9de30f2227e8a",
+  measurementId: "G-L85LF5JSP9"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// // Initialize Firebase Storage
-// const storage = getStorage(app);
+// Initialize Firebase Storage
+const storage = getStorage(app);
 
-// export { storage, db };
+export { db, storage };
+
