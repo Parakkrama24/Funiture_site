6ca0e7e2c6d2fde6fc749dc073cfb423f71071f8
Neshadi <neshadihirunika@gmail.com>
2025-02-19 05:57:36 +0530

    Update Add.jsx

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { assets } from '../../assets/assets';
+import { storage } from '../../FireBase/firebaseConfig';
+import Upload3DModel from '../../firebase/Upload3DModel';
 import './Add.css';
-// import Upload3DModel from '../../firebase/Upload3DModel'; 
-// import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-// import { storage } from '../../FireBase/firebaseConfig';
 
 const Add = ({ url }) => { 
   const [image, setImage] = useState(null); 
